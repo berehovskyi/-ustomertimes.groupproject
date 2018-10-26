4076b20127759f72da85eb2c42c8b0ad7d238e2c
berehovskyi <berehovskyi@gmail.com>
2018-10-26 19:17:28 +0300

    0.0.7 updated Lightning Components: orderList, productList, productListItem and Lightning Controllers: productListController, productListItemController

--- src/aura/productListItem/productListItemController.js
+++ src/aura/productListItem/productListItemController.js
@@ -3,8 +3,20 @@
  */
 ({
     addToCart: function (component, event, helper) {
-        var componentEvent = component.getEvent("addToCart");
+        var componentEvent = component.getEvent('addToCart');
+        var item = component.get("v.item");
+        var inputNumber = component.find('inputNumber').get('v.value');
+        console.log('is number integer? ', 'true');
+        console.log('inputNumber is: ', inputNumber);
+        componentEvent.setParams({
+            "orderItem": {
+                Product__c: item.Id,
+                Name: item.Name,
+                Price__c: item.Price__c,
+                Quantity__c: inputNumber
+            }
+        });
         componentEvent.fire();
         console.log('addToCartEvent has fired');
     }
-})
+});
