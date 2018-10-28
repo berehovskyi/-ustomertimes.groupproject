2f95215619b830d084e03384f8aaebc5b82e8883
berehovskyi <berehovskyi@gmail.com>
2018-10-28 10:14:47 +0200

    1.0.0a alpha release version with high resolution banners

--- src/aura/DeleteAllOrders/DeleteAllOrdersController.js
+++ src/aura/DeleteAllOrders/DeleteAllOrdersController.js
@@ -4,28 +4,22 @@
 ({
     handleClick : function (component, event, helper) {
         var action = component.get("c.deleteAllOrders");
-
-        console.debug('Click on delete');
-
         action.setCallback(this, function (data) {
             var state = data.getState();
-
             var toastEvent = $A.get("e.force:showToast");
             if (state === "SUCCESS") {
-
                 toastEvent.setParams({
                     "title": "Success!",
                     "message": " Your orders have been deleted successfully."
                 });
-
             } else {
                 toastEvent.setParams({
-                    "title": "NO WAY!",
+                    "title": "Error!",
                     "message": " Your orders have not been deleted!!!"
                 });
             }
             toastEvent.fire();
         });
         $A.enqueueAction(action);
-    },
-})
+    }
+});
